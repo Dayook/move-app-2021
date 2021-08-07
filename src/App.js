ab5cc95b39790748e048493b36edab8189458887
Dayook <lidomex@gmail.com>
2021-08-07 23:36:47 +0900

    add moive lists

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import PropTypes from "prop-types";
+import React from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    console.log(movies);
+    this.setState({ movies, isLoading: false });
+  };
+
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  render() {
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading" : "we are ready"}</div>;
+  }
 }
+
+// const foodILike = [
+//   {
+//     id: 1,
+//     name: "Kimchi",
+//     image:
+//       "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg",
+//     rating: 5,
+//   },
+//   {
+//     id: 2,
+//     name: "bulgogi",
+//     image:
+//       "https://www.google.com/search?q=%EB%B6%88%EA%B3%A0%EA%B8%B0&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk02ziUIXmsNvR7TuYKh1cpJGpdpH8g:1593509047151&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi-p87lm6nqAhVbUd4KHVSWBBUQ_AUoAXoECBgQAw&biw=1440&bih=701&dpr=2#imgrc=kjZoAmvHGo-KfM",
+//     rating: 4.9,
+//   },
+//   {
+//     id: 3,
+//     name: "kimbap",
+//     image:
+//       "https://www.google.com/search?q=%EA%B9%80%EB%B0%A5&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk03dn_nrK6M5JlKKfD5krm8w3qTmGQ:1593509069629&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjqn6rwm6nqAhXDQN4KHYLUA4EQ_AUoAXoECBgQAw&biw=1440&bih=701#imgrc=Xz3xQoYZw3bqEM",
+//     rating: 3,
+//   },
+//   {
+//     id: 4,
+//     name: "samgyetang",
+//     image:
+//       "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg",
+//     rating: 4.5,
+//   },
+// ];
+
+// function Food({ name, picture, rating }) {
+//   return (
+//     <div>
+//       <h2>I like {name}</h2>
+//       <h4>{rating}</h4>
+//       <img src={picture} alt={name} />
+//     </div>
+//   );
+// }
+
+// Food.propTypes = {
+//   name: PropTypes.string.isRequired,
+//   picture: PropTypes.string.isRequired,
+//   rating: PropTypes.number.isRequired,
+// };
+
+// function renderFood(dish) {
+//   return <Food name={dish.name} picture={dish.image} />;
+// }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       {console.log(foodILike.map(renderFood))}
+//       {foodILike.map((dish) => (
+//         <Food
+//           key={dish.id}
+//           name={dish.name}
+//           picture={dish.image}
+//           rating={dish.rating}
+//         />
+//       ))}
+//     </div>
+//   );
+// }
 
 export default App;
